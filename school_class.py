fcd9c08ff6d964bf2fb7e919ca63fe5e4ccd73b1
Ask1ng-git <dydoudubg@gmail.com>
2026-03-25 13:02:55 +0100

    add class decorator for matter_4

--- school_class.py
+++ school_class.py
@@ -1,6 +1,16 @@
 from collections.abc import Iterable, Iterator
 
+def add_matter_4(cls):
+    original_init = cls.__init__
 
+    def new_init(self, name, matter_1, matter_2, matter_3, matter_4=0):
+        original_init(self, name, matter_1, matter_2, matter_3)
+        self.matter_4 = matter_4
+
+    cls.__init__ = new_init
+    return cls
+
+@add_matter_4
 class Student:
     def __init__(self, name, matter_1, matter_2, matter_3):
         self.name = name
@@ -8,15 +18,9 @@ class Student:
         self.matter_2 = matter_2
         self.matter_3 = matter_3
 
-
-# Iterator pour matière 1
 class SchoolClassIterator(Iterator):
     def __init__(self, students):
-        self.students = sorted(
-            students,
-            key=lambda student: student.matter_1,
-            reverse=True
-        )
+        self.students = sorted(students, key=lambda s: s.matter_1, reverse=True)
         self.index = 0
 
     def __next__(self):
@@ -26,15 +30,9 @@ class SchoolClassIterator(Iterator):
             return student
         raise StopIteration
 
-
-# Iterator pour matière 2
 class SchoolClassIteratorMatter2(Iterator):
     def __init__(self, students):
-        self.students = sorted(
-            students,
-            key=lambda student: student.matter_2,
-            reverse=True
-        )
+        self.students = sorted(students, key=lambda s: s.matter_2, reverse=True)
         self.index = 0
 
     def __next__(self):
@@ -44,15 +42,9 @@ class SchoolClassIteratorMatter2(Iterator):
             return student
         raise StopIteration
 
-
-# Iterator pour matière 3
 class SchoolClassIteratorMatter3(Iterator):
     def __init__(self, students):
-        self.students = sorted(
-            students,
-            key=lambda student: student.matter_3,
-            reverse=True
-        )
+        self.students = sorted(students, key=lambda s: s.matter_3, reverse=True)
         self.index = 0
 
     def __next__(self):
@@ -61,7 +53,6 @@ class SchoolClassIteratorMatter3(Iterator):
             self.index += 1
             return student
         raise StopIteration
-
 
 class SchoolClass(Iterable):
     def __init__(self):
@@ -70,37 +61,29 @@ class SchoolClass(Iterable):
     def add_student(self, student):
         self.students.append(student)
 
-    # Iterator par défaut → matière 1
     def __iter__(self):
         return SchoolClassIterator(self.students)
 
-    # Iterator matière 2
     def iter_matter_2(self):
         return SchoolClassIteratorMatter2(self.students)
 
-    # Iterator matière 3
     def iter_matter_3(self):
         return SchoolClassIteratorMatter3(self.students)
 
-
 if __name__ == "__main__":
     school_class = SchoolClass()
-    school_class.add_student(Student('J', 10, 12, 13))
-    school_class.add_student(Student('A', 8, 2, 17))
-    school_class.add_student(Student('V', 9, 14, 14))
+    school_class.add_student(Student('J', 10, 12, 13, 15))
+    school_class.add_student(Student('A', 8, 2, 17, 9))
+    school_class.add_student(Student('V', 9, 14, 14, 11))
 
-    print("Matière 1 :")
     for student in school_class:
         print(student.name, student.matter_1)
 
-    print()
-
-    print("Matière 2 :")
     for student in school_class.iter_matter_2():
         print(student.name, student.matter_2)
 
-    print()
-
-    print("Matière 3 :")
     for student in school_class.iter_matter_3():
         print(student.name, student.matter_3)
+
+    for student in school_class.students:
+        print(student.name, student.matter_4)
