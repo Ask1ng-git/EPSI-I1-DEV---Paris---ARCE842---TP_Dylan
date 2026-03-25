5e0deecbc55535f7e4ddcc648b53301b94429873
Ask1ng-git <dydoudubg@gmail.com>
2026-03-25 12:52:37 +0100

    implement Iterable with custom iterator for matter_1 ranking

--- school_class.py
+++ school_class.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterable, Iterator
+
+
 class Student:
     def __init__(self, name, matter_1, matter_2, matter_3):
         self.name = name
@@ -6,39 +9,32 @@ class Student:
         self.matter_3 = matter_3
 
 
-class SchoolClass:
+class SchoolClassIterator(Iterator):
+    def __init__(self, students):
+        self.students = sorted(
+            students,
+            key=lambda student: student.matter_1,
+            reverse=True
+        )
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.students):
+            student = self.students[self.index]
+            self.index += 1
+            return student
+        raise StopIteration
+
+
+class SchoolClass(Iterable):
     def __init__(self):
         self.students = []
 
     def add_student(self, student):
         self.students.append(student)
 
-    def rank_matter_1(self):
-        ranked_students = sorted(
-            self.students,
-            key=lambda student: student.matter_1,
-            reverse=True
-        )
-        for student in ranked_students:
-            print(student.name, student.matter_1)
-
-    def rank_matter_2(self):
-        ranked_students = sorted(
-            self.students,
-            key=lambda student: student.matter_2,
-            reverse=True
-        )
-        for student in ranked_students:
-            print(student.name, student.matter_2)
-
-    def rank_matter_3(self):
-        ranked_students = sorted(
-            self.students,
-            key=lambda student: student.matter_3,
-            reverse=True
-        )
-        for student in ranked_students:
-            print(student.name, student.matter_3)
+    def __iter__(self):
+        return SchoolClassIterator(self.students)
 
 
 if __name__ == "__main__":
@@ -47,8 +43,5 @@ if __name__ == "__main__":
     school_class.add_student(Student('A', 8, 2, 17))
     school_class.add_student(Student('V', 9, 14, 14))
 
-    school_class.rank_matter_1()
-    print()
-    school_class.rank_matter_2()
-    print()
-    school_class.rank_matter_3()
+    for student in school_class:
+        print(student.name, student.matter_1)
