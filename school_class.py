1ae2cfeeae8eb1b1cba97858410591adae82df01
Ask1ng-git <dydoudubg@gmail.com>
2026-03-25 12:46:00 +0100

    add rank_matter_1 method

--- school_class.py
+++ school_class.py
@@ -13,9 +13,20 @@ class SchoolClass:
     def add_student(self, student):
         self.students.append(student)
 
-class Student:
-    def __init__(self, name, matter_1, matter_2, matter_3):
-        self.name = name
-        self.matter_1 = matter_1
-        self.matter_2 = matter_2
-        self.matter_3 = matter_3
+    def rank_matter_1(self):
+        ranked_students = sorted(
+            self.students,
+            key=lambda student: student.matter_1,
+            reverse=True
+        )
+        for student in ranked_students:
+            print(student.name, student.matter_1)
+
+
+if __name__ == "__main__":
+    school_class = SchoolClass()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
+
+    school_class.rank_matter_1()
