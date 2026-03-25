1caea29b58ef6167fcdc313d08ba0f62861e90c6
Ask1ng-git <dydoudubg@gmail.com>
2026-03-25 12:39:21 +0100

    add main test with SchoolClass

--- school_class.py
+++ school_class.py
@@ -12,3 +12,25 @@ class SchoolClass:
 
     def add_student(self, student):
         self.students.append(student)
+
+class Student:
+    def __init__(self, name, matter_1, matter_2, matter_3):
+        self.name = name
+        self.matter_1 = matter_1
+        self.matter_2 = matter_2
+        self.matter_3 = matter_3
+
+
+class SchoolClass:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+
+if __name__ == "__main__":
+    school_class = SchoolClass()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
